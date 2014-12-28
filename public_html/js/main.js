b44ee27fa3b09e04f1451ae895f3417c092e9335
Mihai <Mihai@Mihai.Dlink>
2014-12-28 16:50:47 +0200

    add news / vacantion / article

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,9 +1,22 @@
 $(".slider").bxSlider({
     controls: false,
-    auto: true
+    auto: true,
+    mode: "fade",
+    pause: 10000
 });
 $(".slider-product").bxSlider({
     pager: false,
     auto: true,
     pause: 7500
+});
+$("#upload").change(function () {
+    var fullPath = document.getElementById('upload').value;
+    if (fullPath) {
+        var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+        var filename = fullPath.substring(startIndex);
+        if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+            filename = filename.substring(1);
+        }
+        $("#nameFile").html("Загружено " + filename);
+    }
 });
